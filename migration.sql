3257d3d6583f56d997da0c2f5d492543595abea5
Colin Tolliver <colintolliver@gmail.com>
2023-11-15 15:56:15 -0800

    migration file complete

---
+++ migration.sql
@@ -0,0 +1,36 @@
+CREATE TABLE "User" (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL, -- hashed password
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE Workout_Plan (
+    plan_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "User"(user_id),
+    plan_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE Exercise (
+    exercise_id SERIAL PRIMARY KEY,
+    plan_id INT REFERENCES Workout_Plan(plan_id),
+    exercise_name VARCHAR(255) NOT NULL,
+    sets INT,
+    repetitions INT,
+    weight FLOAT,
+    notes TEXT,
+);
+
+CREATE TABLE Exercise_Log (
+    log_id SERIAL PRIMARY KEY,
+    plan_id INT REFERENCES Workout_Plan(plan_id),
+    exercise_id INT REFERENCES Exercise(exercise_id),
+    user_id INT REFERENCES "User"(user_id),
+    date DATE NOT NULL,
+    sets_completed INT,
+    reps_completed INT,
+    weight_used FLOAT,
+);
